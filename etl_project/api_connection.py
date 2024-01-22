841df6150ce118e4f4d42bcafa4223f96fde09c4
madyark <kozhakhmetovmadyar@gmail.com>
2024-01-22 14:56:24 +0100

    Add metadata class to api_connection.py file

--- etl_project/api_connection.py
+++ etl_project/api_connection.py
@@ -11,6 +11,63 @@ import schedule
 import time
 import logging
 
+class MetaDataLogging:
+    def __init__(
+        self,
+        pipeline_name: str,
+        postgresql_client: PostgreSqlClient,
+        config: dict = {},
+        log_table_name: str = "pipeline_logs",
+    ):
+        self.pipeline_name = pipeline_name
+        self.log_table_name = log_table_name
+        self.postgresql_client = postgresql_client
+        self.config = config
+        self.metadata = MetaData()
+        self.table = Table(
+            self.log_table_name,
+            self.metadata,
+            Column("pipeline_name", String, primary_key=True),
+            Column("run_id", Integer, primary_key=True),
+            Column("timestamp", String, primary_key=True),
+            Column("status", String, primary_key=True),
+            Column("config", JSON),
+            Column("logs", String),
+        )
+        self.run_id: int = self._get_run_id()
+
+    def _create_log_table(self) -> None:
+        """Create log table if it does not exist."""
+        self.postgresql_client.create_all_tables(metadata=self.metadata)
+
+    def _get_run_id(self):
+        """Gets the next run id. Sets run id to 1 if no run id exists."""
+        self._create_log_table()
+        run_id = self.postgresql_client.engine.execute(
+            select(func.max(self.table.c.run_id)).where(
+                self.table.c.pipeline_name == self.pipeline_name
+            )
+        ).first()[0]
+        if run_id is None:
+            return 1
+        else:
+            return run_id + 1
+
+    def log(self, status:str, timestamp:datetime, logs:str) -> None:
+        """Writes pipeline metadata log to a database"""
+        if timestamp is None:
+            timestamp = datetime.now()
+        insert_statement = insert(self.table).values(
+            pipeline_name=self.pipeline_name,
+            timestamp=timestamp,
+            run_id=self.run_id,
+            status=status,
+            config=self.config,
+            logs=logs,
+        )
+        self.postgresql_client.engine.execute(insert_statement)
+
+
 class PipelineLogging:
     """
     Creates logging object with specific format and file name to log pipeline run. 
@@ -489,11 +546,12 @@ if __name__ == "__main__":
     sql_folder_path = "etl_project/sql" 
     log_folder_path = "etl_project/logs"
     pipeline_name = "Chicago Crime ETL"
+    log_table_name = "pipeline_logs"
 
     # Instantiating logger for pipeline run
     pipeline_logging = PipelineLogging(pipeline_name=pipeline_name, log_folder_path=log_folder_path)
 
-    # Try-except to log pipeline run error  
+    # Try-except to log any errors during pipeline run
     try:
         pipeline_start_time = time.time()
         pipeline_logging.logger.info("Pipeline start")
